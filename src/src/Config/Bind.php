5cf33acfdc418ae50a7c39db5ef9a24ac74477f4
baisnriqpl <baisnriqpl@126.com>
2022-09-29 19:09:45 +0800

    新增数据验证器

--- src/src/Config/Bind.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-    return [
-        \Alex\Annotations\Services\Autoload::class,
-        \Alex\Annotations\Services\Autowired::class,
-        \Alex\Annotations\Services\Validate\Validated::class
-    ];
--- src/src/Interfaces/ServicePropertyInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Alex\Annotations\Interfaces;
-use ReflectionProperty;
-
-interface ServicePropertyInterface
-{
-    public function handle(ReflectionProperty $property, Object $class);
-}
